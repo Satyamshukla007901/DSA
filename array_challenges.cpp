1b925d63100ddedbc401e4c050f4cd0527d27f55
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-08-18 01:46:12 +0530

    added a function to find first and last occurence of an array

--- array_challenges.cpp
+++ array_challenges.cpp
@@ -65,20 +65,54 @@ void rotate(int arr[], int n)
         }
     }
 }
+// First and last occurrences of x
+vector<int> find(int arr[], int n , int x )
+{
+    int start=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            start=i;
+            break;
+        }
+    }
+    int end=-1;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(arr[i]==x)
+        {
+            end=i;
+            break;
+        }
+    }
+    vector<int> vp;
+    vp.push_back(start);
+    vp.push_back(end);
+    return vp;
+    // code here
+}
 int main() {
     int N;
     cin>>N;
-    vector<int> vp;
+    int vp[N];
     for(int i=0;i<N;i++)
     {
-        int x;
-        cin>>x;
-        vp.push_back(x);
+        // int x;
+        // cin>>x;
+        // vp.push_back(x);
+        cin>>vp[i];
     }
-    // vp = separateNegativeAndPositive(vp);
-    for(auto &value:vp)
-        cout<<value<<" ";
-    cout<<endl;
+    // // vp = separateNegativeAndPositive(vp);
+    // for(auto &value:vp)
+    //     cout<<value<<" ";
+    // cout<<endl;
+    // vector<int> A = findf(vp,N,2);
+    // for(auto &value:A)
+    // {
+    //     cout<<value<<" ";
+    // }
+    // cout<<endl;
 	// your code goes here
 	return 0;
 }
