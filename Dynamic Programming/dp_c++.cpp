62b7fbbd14243ba98f95e4bfb1348a222498bdb5
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-15 22:41:12 +0530

    Perfect sum subset

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -89,3 +89,32 @@ int equalPartition(int N, int arr[])
     return dp[N][sum];
     // code here
 }
+//Perfect Sum Problem
+//https://practice.geeksforgeeks.org/problems/perfect-sum-problem5633/1#
+
+int perfectSum(int arr[], int n, int sum)
+{
+    int dp[n + 1][sum + 1];
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= sum; j++)
+            dp[i][j] = 0;
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+    for (int j = 1; j <= sum; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= sum; j++)
+        {
+            if (arr[i - 1] > j)
+                dp[i][j] = dp[i - 1][j] % 1000000007;
+            else
+                dp[i][j] = (dp[i - 1][j] % 1000000007) + (dp[i - 1][j - arr[i - 1]] % 1000000007) % 1000000007;
+        }
+    }
+    return dp[n][sum] % 1000000007;
+
+    // Your code goes here
+}
