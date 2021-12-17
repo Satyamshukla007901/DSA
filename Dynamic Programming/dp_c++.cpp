2fef80c0959b989d9a0f3da97833064647262984
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-17 15:22:50 +0530

    coin problem added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -269,4 +269,67 @@ int knapSack(int N, int W, int val[], int wt[])
     return dp[N][W];
     // code here
 }
-/
+//Rod cutting problem
+//https://practice.geeksforgeeks.org/problems/rod-cutting0840/1
+int cutRod(int price[], int n)
+{
+    int length[n];
+    for (int i = 0; i < n; i++)
+        length[i] = i + 1;
+
+    int dp[n + 1][n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (length[i - 1] <= j)
+            {
+                dp[i][j] = max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
+            }
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    return dp[n][n];
+    //code here
+}
+//Coin Problem
+//https://practice.geeksforgeeks.org/problems/coin-change2448/1#
+long long int count(int S[], int m, int n)
+{
+
+    long long dp[m + 1][n + 1];
+    for (int j = 1; j <= n; j++)
+        dp[0][j] = 0;
+    for (int i = 0; i <= m; i++)
+    {
+        dp[i][0] = 1;
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (S[i - 1] <= j)
+            {
+                dp[i][j] = dp[i][j - S[i - 1]] + dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[m][n];
+    // code here.
+}
