28a8ca67d8862ae848b15bbd6b33509308b35eef
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-18 16:59:23 +0530

    LCS added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -368,3 +368,34 @@ int minCoins(int coins[], int M, int V)
     return dp[M][V];
     // Your code goes here
 }
+//https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1
+//Function to find the length of longest common subsequence in two strings.
+int lcs(int x, int y, string s1, string s2)
+{
+    int dp[x + 1][y + 1];
+    for (int i = 0; i <= x; i++)
+    {
+        for (int j = 0; j <= y; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= y; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[x][y];
+    // your code here
+}
