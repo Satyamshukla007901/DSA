a5c75416d70135c599089b2bd32a6a370d6e3112
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-19 13:36:09 +0530

    longest common substring added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -399,3 +399,37 @@ int lcs(int x, int y, string s1, string s2)
     return dp[x][y];
     // your code here
 }
+//Longest Common Substring
+//https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1
+int longestCommonSubstr(string S1, string S2, int n, int m)
+{
+    int dp[n + 1][m + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (S1[i - 1] == S2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                ans = max(ans, dp[i][j]);
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    return ans;
+    // your code here
+}
