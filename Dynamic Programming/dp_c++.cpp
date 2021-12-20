7794cb2ea11480b2ded2371642d2bb081a32d61f
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-20 14:53:23 +0530

    SCS Problem added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -434,3 +434,74 @@ int longestCommonSubstr(string S1, string S2, int n, int m)
     return ans;
     // your code here
 }
+//Longest Common Subsequence Print
+string printLcs(string s1, string s2, int n, int m)
+{
+    int dp[n + 1][m + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    ll i = n;
+    ll j = m;
+    string ans = "";
+    while (i > 0 && j > 0)
+    {
+        if (s1[i - 1] == s2[j - 1])
+        {
+            ans += s1[i - 1];
+            i--;
+            j--;
+        }
+        else
+        {
+            if (dp[i - 1][j] > dp[i][j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+    return ans;
+}
+//Shortest Common Super Sequence
+//https://practice.geeksforgeeks.org/problems/shortest-common-supersequence0322/1
+int shortestCommonSupersequence(string X, string Y, int m, int n)
+{
+
+    long long sz = 0;
+    int x;
+    string ans = printLcs(X, Y, m, n, x);
+    sz = x;
+    auto it = X.find(ans);
+    sz += it;
+    sz += (X.length() - (it + x));
+    it = Y.find(ans);
+    sz += it;
+    sz += (Y.length() - (it + x));
+    return sz;
+
+    //code here
+}
