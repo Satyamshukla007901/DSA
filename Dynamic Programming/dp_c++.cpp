e02a69b6e602f92205e0862af207031aba43a2eb
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-21 21:39:38 +0530

    Longest Repeating Subsequence added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -591,3 +591,34 @@ string printshortestCommonSupersequence(string str1, string str2)
     reverse(ans.begin(), ans.end());
     return ans;
 }
+//Longest Repeating Subsequence
+//https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1#
+int LongestRepeatingSubsequence(string str)
+{
+    int x = str.length(), y = str.length();
+    int dp[x + 1][y + 1];
+    for (int i = 0; i <= x; i++)
+    {
+        for (int j = 0; j <= y; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= y; j++)
+        {
+            if (str[i - 1] == str[j - 1] && i != j)
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[x][y];
+}
