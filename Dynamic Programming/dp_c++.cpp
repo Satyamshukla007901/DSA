4bdf2d273d2a3c2576760f9eac2ac51541f9e6c9
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-23 12:54:28 +0530

    MCM bottom up added

--- Dynamic Programming/dp_c++.cpp
+++ Dynamic Programming/dp_c++.cpp
@@ -690,3 +690,29 @@ int matrixMultiplication(int N, int arr[])
     return solve(arr, i, j);
     // code here
 }
+//Memoisation Bottom Up Approach
+int dp[1001][1001];
+int solve(int arr[], int i, int j)
+{
+    if (i >= j)
+        return 0;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+    int ans = INT_MAX;
+    for (int k = i; k <= j - 1; k++)
+    {
+        int tempans = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+        if (tempans < ans)
+            ans = tempans;
+    }
+    return dp[i][j] = ans;
+}
+int matrixMultiplication(int N, int arr[])
+{
+    memset(dp, -1, sizeof(dp));
+    int i = 1;
+    int j = N - 1;
+
+    return solve(arr, i, j);
+    // code here
+}
