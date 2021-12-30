6e7ea332a3bc7a328f5c3302c26cb0e5c8d981ae
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-30 12:55:07 +0530

    two more problems added

--- Binary Search/binary_search_c++.cpp
+++ Binary Search/binary_search_c++.cpp
@@ -137,3 +137,76 @@ int findKRotation(int arr[], int n)
 
     // code here
 }
+//Search in Rotated Sorted Array
+//https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+int pivot(vector<int> vp, int n)
+{
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (vp[(mid - 1 + n) % n] > vp[mid] && vp[mid] < vp[(mid + 1) % n])
+            return mid;
+        else if (vp[mid] >= vp[0])
+            s = mid + 1;
+        else
+            e = mid - 1;
+    }
+    return 0;
+}
+
+int bSearch(vector<int> vp, int start, int end, int target)
+{
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (vp[mid] == target)
+            return mid;
+        else if (vp[mid] < target)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return -1;
+}
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int idx = pivot(nums, nums.size());
+    if (n == 1)
+    {
+        if (nums[0] == target)
+            return 0;
+        return -1;
+    }
+
+    if (idx != -1 && target >= nums[idx] && target <= nums[n - 1])
+    {
+        return bSearch(nums, idx, n - 1, target);
+    }
+    else
+        return bSearch(nums, 0, idx - 1, target);
+    return -1;
+}
+//Search in Nearly Sorted Array
+int Search(vector<int> arr,int n,int x)
+{
+    int s = 0;
+    int e = n-1;
+    while(s<=e)
+    {   
+        int mid = s + (e-s)/2;
+        if(arr[mid]==x)
+            return mid;
+        else if(mid-1>=s&&arr[mid-1]==x)
+            return mid-1;
+        else if(mid+1<=e&&arr[mid+1]==x)
+            return mid+1;
+        else if(arr[mid]<=x)
+            start = mid+2;
+        else
+            end=mid-2;
+    }
+    return -1;
+}
