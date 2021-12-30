e610b8015482488794ac4300b56ceceb35186e50
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-30 17:43:07 +0530

    next greater letter added

--- Binary Search/binary_search_c++.cpp
+++ Binary Search/binary_search_c++.cpp
@@ -233,3 +233,24 @@ int findFloor(vector<long long> v, long long n, long long x)
 
     // Your code here
 }
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/
+//Next greater letter
+char nextGreatestLetter(vector<char> &letters, char target)
+{
+    int s = 0;
+    int n = letters.size();
+    int e = n - 1;
+    char ans = letters[0];
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (letters[mid] > target)
+        {
+            ans = letters[mid];
+            e = mid - 1;
+        }
+        else
+            s = mid + 1;
+    }
+    return ans;
+}
