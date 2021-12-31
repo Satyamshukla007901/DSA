e385560b69ede49037f1092383cdd28f7b10b659
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-31 22:47:57 +0530

    peak element index problem added

--- Binary Search/binary_search_c++.cpp
+++ Binary Search/binary_search_c++.cpp
@@ -254,3 +254,29 @@ char nextGreatestLetter(vector<char> &letters, char target)
     }
     return ans;
 }
+//find peak element index
+//https://leetcode.com/problems/find-peak-element/submissions/
+int findPeakElement(vector<int> &nums)
+{
+
+    int n = nums.size();
+    if (n == 1)
+        return 0;
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (mid - 1 >= 0 && mid + 1 < n && nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+            return mid;
+        else if (mid + 1 < n && nums[mid] < nums[mid + 1])
+        {
+            low = mid + 1;
+        }
+        else
+            high = mid - 1;
+    }
+    if (nums[0] > nums[1])
+        return 0;
+    return n - 1;
+}
