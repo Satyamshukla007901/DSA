38edd599b1375a92bedc2037c7fbfad647bc5d29
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-03 13:12:48 +0530

    sort an array problem added

--- Recursion/recursion_c++.cpp
+++ Recursion/recursion_c++.cpp
@@ -7,3 +7,25 @@ void printN(int N)
     printN(N-1);
     cout<<N<<" ";
 }
+//Sort an Array
+void insert(vector<int> &vp,int ok)
+{
+    if(vp.size()==0||vp[vp.size()-1]<=ok)
+    {
+        vp.push_back(ok);
+        return;
+    }
+    int val = vp[vp.size()-1];
+    vp.pop_back();
+    insert(vp,ok);
+    vp.push_back(val);
+}
+void sort(vector<int> &vp)
+{
+    if(vp.size()==1)
+        return;
+    int temp = v[v.size()-1];
+    v.pop_back();
+    sort(vp);
+    insert(v,temp);
+}
