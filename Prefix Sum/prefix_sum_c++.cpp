7e464afc010195377266eb44b1e63cc81bbf1ffa
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-09 22:20:56 +0530

    prefix sum folder and first problemn added  problem another improvement  added

--- Prefix Sum/prefix_sum_c++.cpp
+++ Prefix Sum/prefix_sum_c++.cpp
@@ -4,24 +4,39 @@ using namespace std;
 //https://practice.geeksforgeeks.org/problems/equal-sum0810/1/
 string equilibrium(int *arr, int n)
 {
-    vector<int> left(n, 0);
-    vector<int> right(n, 0);
-    left[0] = arr[0];
-    for (int i = 1; i < n; i++)
-    {
-        left[i] = left[i - 1] + left[i] + arr[i];
-    }
-    right[n - 1] = arr[n - 1];
-    for (int i = n - 2; i >= 0; i--)
-    {
-        right[i] = right[i] + right[i + 1] + arr[i];
-    }
+    /* Space Complexity is O(2N)
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        left[0] = arr[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i]=left[i-1]+left[i]+arr[i];
+        }
+        right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=right[i]+right[i+1]+arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(left[i]==right[i])
+            {
+                return "YES";
+            }
+        }
+        return "NO";*/
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+
+    int left = 0;
     for (int i = 0; i < n; i++)
     {
-        if (left[i] == right[i])
+        if (left == sum - left - arr[i])
         {
             return "YES";
         }
+        left += arr[i];
     }
     return "NO";
     // code here
