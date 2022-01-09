b7112f05ebad341546db05748276af3dd6d29730
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-09 22:07:20 +0530

    prefix sum folder and first problemn added  problem added

---
+++ Prefix Sum/prefix_sum_c++.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Equal Sum
+//https://practice.geeksforgeeks.org/problems/equal-sum0810/1/
+string equilibrium(int *arr, int n)
+{
+    vector<int> left(n, 0);
+    vector<int> right(n, 0);
+    left[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        left[i] = left[i - 1] + left[i] + arr[i];
+    }
+    right[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        right[i] = right[i] + right[i + 1] + arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (left[i] == right[i])
+        {
+            return "YES";
+        }
+    }
+    return "NO";
+    // code here
+}
+int32_t main()
+{
+    return 0;
+}
