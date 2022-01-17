99e642b2e118e3770991bab5305b40430962f5d5
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-17 13:22:25 +0530

    bs new problem added

--- Binary Search/binary_search_c++.cpp
+++ Binary Search/binary_search_c++.cpp
@@ -503,3 +503,50 @@ public:
         return ans;
     }
 };
+//single element in a sorted array
+//https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int s = 0;
+        int e = nums.size()-1;
+        if(nums.size()==1)
+                return nums[0];
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+            if(mid-1>=0&&mid+1<nums.size()&&nums[mid-1]!=nums[mid]&&nums[mid+1]!=nums[mid])
+            {
+                return nums[mid];
+            }
+            else if(mid+1<nums.size()&&mid==0&&nums[mid]!=nums[mid+1])
+            {
+                return nums[mid];
+            }
+            else if(mid-1>=0&&mid==nums.size()-1&&nums[mid-1]!=nums[mid])
+            {
+                return nums[mid];
+            }
+            //Cacth of the Problem :-  first occurence of every duyplicate is at even index before single occurence element and first occurence of every duplicate is at odd index after single occurence elemnt
+            else if(mid-1>=0&&nums[mid-1]==nums[mid])
+            {
+                if((mid-1)%2==0)
+                {
+                    s = mid+1;
+                }
+                else
+                    e = mid-1;
+            }
+            else
+            {
+                if((mid+1)%2==0)
+                {
+                    e = mid-1;
+                }
+                else
+                    s = mid+1;
+            }
+        }
+        return 0;
+    }
+};
