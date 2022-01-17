267ae96dcb80d4f7f6adbd1b6a83119d2009c362
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-17 12:52:11 +0530

    interactive bs added

--- Binary Search/binary_search_c++.cpp
+++ Binary Search/binary_search_c++.cpp
@@ -453,3 +453,53 @@ int cubeRoot(int N)
     return 0;
     // code here
 }
+//Interactive Binary Search problem
+//https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/submissions/
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution
+{
+public:
+    vector<vector<int>> findSolution(CustomFunction &customfunction, int z)
+    {
+        vector<vector<int>> ans;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            int x = i;
+            int s = 1;
+            int e = 1000;
+            while (s <= e)
+            {
+                int mid = s + (e - s) / 2;
+                if (customfunction.f(x, mid) == z)
+                {
+                    vector<int> temp(2);
+                    temp[0] = x;
+                    temp[1] = mid;
+                    ans.push_back(temp);
+                    break;
+                }
+                else if (customfunction.f(x, mid) < z)
+                {
+                    s = mid + 1;
+                }
+                else
+                {
+                    e = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
