4a5274b211613dc527c6cb388643ccec916e1c85
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-01-22 13:13:07 +0530

    bs answer added

--- Binary Search/binary_search_on_answer_.cpp
+++ Binary Search/binary_search_on_answer_.cpp
@@ -206,3 +206,50 @@ public:
         return ans;
     }
 };
+//Median of two sorted arrays
+//https://leetcode.com/problems/median-of-two-sorted-arrays/
+//striver qpproach
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums2.size()<nums1.size())
+                return findMedianSortedArrays(nums2, nums1);
+        int n = nums1.size();
+        int m = nums2.size();
+
+        
+        int s = 0;
+        int e = n;
+        while(s<=e)
+        {
+            int cut1 = (s+e)>>1;
+            int cut2  = (n+m+1)/2 - cut1;
+            
+            int left1 = (cut1==0)?INT_MIN:nums1[cut1-1];
+            int left2 = (cut2==0)?INT_MIN:nums2[cut2-1];
+            
+            
+            int right1 = (cut1==n)?INT_MAX:nums1[cut1];
+            int right2 = (cut2==m)?INT_MAX:nums2[cut2];
+            
+            if(left1<=right2&&left2<=right1)
+            {
+                if(((n+m)%2)==0)
+                {
+                    return (max(left1,left2)+min(right1,right2))/2.0;
+                }
+                else
+                {
+                    return max(left1,left2);
+                }
+            }
+            else if(left1>right2)
+            {
+                e = cut1-1;
+            }
+            else
+                s=cut1+1;
+        }
+        return 0.0;
+    }
+};
