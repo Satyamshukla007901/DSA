9c118d684321f0cd041131968abf42bf31e29b6a
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2022-02-21 13:22:38 +0530

    added C problem

--- Competitve Programming Challenges/cf_div2_2/21/2022.cpp
+++ Competitve Programming Challenges/cf_div2_2/21/2022.cpp
@@ -59,3 +59,63 @@ void solve()
     }
     cout<<endl;
 }
+
+//C - https://codeforces.com/contest/1635/problem/C
+void solve()
+{
+    ll N;
+    cin>>N;
+    vector<ll> vp;
+    for(int i=0;i<N;i++)
+    {
+        ll X;
+        cin>>X;
+        vp.push_back(X);
+    }
+    ll cnt = 0;
+    vector<vector<ll>> ok;
+    if(vp[N-2]>vp[N-1])
+    {
+        cout<<-1<<endl;return;
+    }
+    ll a = vp[N-2];
+    ll adx = N-2;
+    ll b = vp[N-1];
+    ll bdx = N-1;
+    for(int i=N-3;i>=0;i--)
+    {
+        if(i+1<N&&vp[i]>vp[i+1])
+        {
+            vp[i]=a-b;
+            cnt++;
+            ok.push_back({i+1,adx+1,bdx+1});
+        }
+        if(vp[i]<a)
+        {
+            a=vp[i];
+            adx=i;
+        }
+        if(vp[i]>b)
+        {
+            b=vp[i];
+            bdx=i;
+        }
+    }
+    // for(auto &value:vp)
+    // {
+    //     cout<<value<<" ";
+    // }
+    // cout<<endl;
+    for(int i=0;i<N;i++)
+    {
+        if(i+1<N&&vp[i]>vp[i+1])
+        {
+            cout<<-1<<endl;return;
+        }
+    }
+    cout<<cnt<<endl;
+    for(auto &value:ok)
+    {
+        cout<<value[0]<<" "<<value[1]<<" "<<value[2]<<endl;
+    }
+}
